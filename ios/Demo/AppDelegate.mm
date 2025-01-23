3876e6c475cbae27dab603065089faa8dac392f7
BitcoinZavior <BitcoinZavior@GMail.Com>
2025-01-23 00:06:00 -0500

    Update .gitignore to include .yarn and specify Xcode project in Podfile

--- ios/Demo/AppDelegate.mm
+++ ios/Demo/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"RNDemo";
+  self.moduleName = @"Demo";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
@@ -15,11 +15,6 @@
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-  return [self bundleURL];
-}
-
-- (NSURL *)bundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
